43621f8d1164b09aee90462ec829d3b8f8a3d5a9
gapalaci <gapalaci@gapalaci.gapalaci>
2021-06-30 20:22:45 -0500

    updating root installer

--- install_root.sh
+++ install_root.sh
@@ -4,8 +4,8 @@ cd $workdir
 mkdir root_v6
 cd $workdir
 ###
-wget https://root.cern/download/root_v6.24.02.source.tar.gz
-tar zxf root_v6.24.02.source.tar.gz
+wget https://root.cern/download/root_v6.24.00.source.tar.gz
+tar zxf root_v6.24.00.source.tar.gz
 cd $workdir/root_v6
-cmake -$workdir/root-6.24.02
-make -j2
+cmake $workdir/root-6.24.00
+make -j4
